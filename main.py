3352ee6671f4a6bdfba75c6fad7056c0d5c66f53
anton <anton.dorosev@yandex.ru>
2020-11-28 22:52:10 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from random import randint
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("UI.ui", self)
+        self.initUI()
+        self.coords = [150, 150]
+        self.s = 0
+        self.color = QColor(255, 255, 0)
+        self.flag = False
+
+    def initUI(self):
+        self.setWindowTitle('Git и желтые окружности')
+        self.btn_draw.clicked.connect(self.rands)
+        self.show()
+
+    def rands(self):
+        self.s = randint(1, 200)
+        self.paint()
+
+    def paint(self):
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw()
+            self.qp.end()
+
+    def draw(self):
+        self.qp.setBrush(self.color)
+        self.qp.drawEllipse(self.coords[0] - self.s // 2, self.coords[1] - self.s // 2, self.s, self.s)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
